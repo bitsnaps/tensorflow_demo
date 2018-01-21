b4ab653d975ef9adcc049da495b86b783fea07fb
bitsnaps <bitsnaps@yahoo.fr>
2018-01-21 20:25:18 +0100

    Update _read32 function

--- input_data.py
+++ input_data.py
@@ -20,10 +20,12 @@ def maybe_download(filename, work_directory):
     return filepath
 
 
+#def _read32(bytestream):
+#    dt = numpy.dtype(numpy.uint32).newbyteorder('>')
+#    return numpy.frombuffer(bytestream.read(4), dtype=dt)
 def _read32(bytestream):
     dt = numpy.dtype(numpy.uint32).newbyteorder('>')
-    return numpy.frombuffer(bytestream.read(4), dtype=dt)
-
+    return numpy.asscalar(numpy.frombuffer(bytestream.read(4), dtype=dt))
 
 def extract_images(filename):
     """Extract the images into a 4D uint8 numpy array [index, y, x, depth]."""
